b3d98323827531c30f2a8e732165e69c8da38616
Gabriel Campos Lima Alves <campos.gabriel@estudante.ufjf.br>
2022-11-08 13:25:36 -0300

    atualiza estilos

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -1,5 +1,27 @@
+import {FaFacebook, FaInstagram, FaLinkedin, FaTwitter} from "react-icons/fa";
+import styles from "./Contact.module.css";
+import  Container  from "../layout/Container/Container";
+
 function Contact (){
-    return <h1>Contact</h1>
+    return (
+        <div className={styles.contact_container}>
+            <p>Nossas redes</p>
+            <ul className={styles.contact_list}>
+                <li>
+                    <FaFacebook/>
+                </li>
+                <li>
+                    <FaInstagram/>
+                </li>
+                <li>
+                    <FaLinkedin/>
+                </li>
+                <li>
+                    <FaTwitter/>
+                </li>
+            </ul>
+        </div>
+    )
 }
 
 export default Contact ;
